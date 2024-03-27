efb0f1c0c50a7ac8289abdb55a8dc969ac004baf
alvega-g <alvarvg@gmail.com>
2024-03-27 20:25:02 +0100

    cmds & redirect done; env todo

--- src/main.c
+++ src/main.c
@@ -135,25 +135,105 @@ Resurces read/seen
 
 */
 
+int ft_determine_in_fd(char *redirect)
+{
+	int fd;
+	int i;
+	char *str;
 
-t_cmd *ft_setup_cmds(char **cmds)
+	redirect = ft_strchr(redirect, ':');
+	redirect++;
+	i = -1;
+	while (redirect[++i] != ' ')
+		;
+	str = ft_substr(redirect, 0, i);
+	if (ft_isdigit(*redirect))
+		fd = ft_atoi(redirect);
+	else
+		fd = open(str, O_RDONLY);
+	free(str);
+	return (fd);
+}
+
+int ft_determine_out_fd(char *redirect)
+{
+	int fd;
+
+	redirect = ft_strrchr(redirect, ':');
+	redirect++;
+	if (ft_isdigit(*redirect))
+		fd = ft_atoi(redirect);
+	else if (ft_strnstr(redirect, "(A)", ft_strlen(redirect)))
+		fd = open(redirect, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else
+		fd = open(redirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	return (fd);
+}
+
+t_cmd *ft_fill_nodes(t_cmd *head, char **cmds, char **redirect)
+{
+	int i;
+	int j;
+	t_cmd *current;
+
+	i = -1;
+	current = head;
+	while (current)
+	{
+		current->cmd = ft_strdup(cmds[++i]);
+		current->args = NULL;
+		j = -1;
+		while (cmds[i][++j] != ' ')
+			;
+		current->args = ft_strdup(&cmds[i][j + 1]);
+		current->redirect->in_fd = ft_determine_in_fd(redirect[i]);
+		current->redirect->out_fd = ft_determine_out_fd(redirect[i]);
+		current = current->next;
+	}
+	return (head);
+}
+
+t_cmd *ft_setup_nodes(char **cmds, char **redirect)
 {
 	int i;
 	t_cmd *current;
-	t_cmd **head;
+	t_cmd *new_node;
+	t_cmd *head;
 
-	head = &current;
+	head = NULL;
 	i = -1;
 	while (cmds[++i])
 	{
-		current = malloc(sizeof(t_cmd));
-		if (!current)
+		new_node = malloc(sizeof(t_cmd));
+		if (!new_node)
 			return (NULL);
-		current->cmd = ft_strdup(cmds[i]);
+		new_node->redirect = malloc(sizeof(t_redirect));
+		if (!new_node->redirect)
+			return (NULL);
+		new_node->next = NULL;
+		if (head == NULL)
+			head = new_node;
+		else
+			current->next = new_node;
+		current = new_node;
+	}
+	head = ft_fill_nodes(head, cmds, redirect);
+	return (head);
+}
+
+void ft_debug_cmds(t_cmd *cmds)
+{
+	t_cmd *current;
+
+	current = cmds;
+	while (current)
+	{
+		printf("\ncmd: %s\n", current->cmd);
+		printf("args: %s\n", current->args);
+		printf("in_fd: %d\n", current->redirect->in_fd);
+		printf("out_fd: %d\n", current->redirect->out_fd);
 		current = current->next;
 	}
-	current->next = NULL;
-	return (*head);
 }
 
 int ft_parsing_loop(t_data *data)
@@ -162,13 +242,15 @@ int ft_parsing_loop(t_data *data)
 	char **cmds;
 	char **redirect;
 
-	prompt = readline("minishell$ ");
+	// prompt = readline("minishell$ ");
 	// prompt = ft_strdup("cat -e < infile.txt | ls -lah >> outfile.txt");
+	prompt = ft_strdup("cat -e | ls -lah");
 	cmds = ft_parsing(prompt);
 	redirect = ft_redirections(cmds);
 	cmds = ft_clean_cmds(cmds);
 
-	data->cmds = ft_setup_cmds(cmds);
+	data->cmds = ft_setup_nodes(cmds, redirect);
+	ft_debug_cmds(data->cmds);
 
 	ft_clean_double_ptr(cmds);
 	ft_clean_double_ptr(redirect);
