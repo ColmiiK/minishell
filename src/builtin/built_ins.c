0f2c1101d6b23bb550186c42109a4dfc3612ef0e
Alba <alba.gar.diaz@gmail.com>
2024-05-09 16:59:28 +0200

    added : exit status

--- src/builtin/built_ins.c
+++ src/builtin/built_ins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   built_ins.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: albagar4 <albagar4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:56:45 by albagar4          #+#    #+#             */
-/*   Updated: 2024/05/09 16:22:43 by alvega-g         ###   ########.fr       */
+/*   Updated: 2024/05/09 16:58:56 by albagar4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,21 @@ int	built_in_checker(char *str)
 		return (0);
 }
 
-void	built_in_selector(t_data *data, char **str)
+int	built_in_selector(t_data *data, char **str)
 {
 	if (!ft_strncmp(str[0], "echo", 5))
-		ft_echo(str);
+		data->exit_status = ft_echo(str);
 	else if (!ft_strncmp(str[0], "cd", 3))
-		ft_cd(str, &data->env);
+		data->exit_status = ft_cd(str, &data->env);
 	else if (!ft_strncmp(str[0], "pwd", 4))
-		ft_pwd();
+		data->exit_status = ft_pwd();
 	else if (!ft_strncmp(str[0], "export", 7))
-		ft_export(&data->env, str[1]);
+		data->exit_status = ft_export(&data->env, str[1]);
 	else if (!ft_strncmp(str[0], "unset", 6))
-		ft_unset(&data->env, str[1]);
+		data->exit_status = ft_unset(&data->env, str[1]);
 	else if (!ft_strncmp(str[0], "env", 4))
-		ft_env(&data->env);
+		data->exit_status = ft_env(&data->env);
 	else if (!ft_strncmp(str[0], "exit", 5))
-		ft_exit(str);
+		data->exit_status = ft_exit(str);
+	return (data->exit_status);
 }
